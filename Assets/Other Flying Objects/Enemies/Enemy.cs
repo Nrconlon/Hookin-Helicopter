cf4a10746775978d38a61e9ef3dc8aa52f8ee8ec
Neil Conlon <nrconlon@gmail.com>
2017-10-21 11:51:46 -0700

    set up scripts

---
+++ Assets/Other Flying Objects/Enemies/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : FlyingObject {
+	PlayerHelicopter playerHelicopter;
+	// Use this for initialization
+	void Start () {
+		playerHelicopter = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHelicopter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Other Flying Objects/FlyingObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class FlyingObject : MonoBehaviour {
+	bool isHooked = false;
+	Rigidbody2D myRigidBody;
+
+
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+		myRigidBody.Sleep();
+	}
+	
+	void Update () {
+		
+	}
+
+	public virtual void GotHooked()
+	{
+		isHooked = true;
+		myRigidBody.WakeUp();
+	}
+
+	public virtual void UnHooked()
+	{
+		isHooked = false;
+		myRigidBody.Sleep();
+	}
+}
