d248a1fadaea0815f5fc0001e1411590a056be68
Tate Conlon <tate.conlon@gmail.com>
2017-10-31 22:52:22 -0400

    ROPE IS TWERKIN'

--- Assets/Helicopter/Hook/Hook.cs
+++ Assets/Helicopter/Hook/Hook.cs
@@ -2,20 +2,10 @@
 
 public class Hook : MonoBehaviour
 {
-
-	public float distanceFromChainEnd = 0.6f;
 	HookableObject hookedObject;
 	[SerializeField] float reHookDelay = 0.5f;
 	float reHookDelayTimer = 0f;
 
-	public void ConnectRopeEnd(Rigidbody2D endRB)
-	{
-		HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
-		joint.autoConfigureConnectedAnchor = false;
-		joint.connectedBody = endRB;
-		joint.anchor = Vector2.zero;
-		joint.connectedAnchor = new Vector2(0f, -distanceFromChainEnd);
-	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(hookedObject)
--- Assets/Helicopter/Hook/Rope.cs
+++ Assets/Helicopter/Hook/Rope.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour {
 
-	public Rigidbody2D helicoper;
-
+	public Rigidbody2D helicopterRB;
+	public Transform helicopterRopeAnchorPoint;
+	public GameObject hookPrefab;
 	public GameObject linkPrefab;
 
-	public Hook hook;
+	[SerializeField] float ropeLength = 2.1f;
 
-	[SerializeField] float helicopterMass = 200f;
+	[SerializeField] int numLinks = 7;
+	[SerializeField] float linkMass = 100;
 
-	public int links = 7;
+	private Hook hook;
 
 	void Start () {
 		GenerateRope();
@@ -18,30 +20,47 @@ public class Rope : MonoBehaviour {
 
 	void GenerateRope ()
 	{
-		Rigidbody2D previousRB = helicoper;
-		for (int i = 0; i < links; i++)
+		float linkDist = ropeLength / numLinks;
+		Rigidbody2D prevRB = helicopterRB;
+
+		for (int i = 0; i < numLinks-1; i++)
 		{
-			GameObject link = Instantiate(linkPrefab, transform);
-			HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
-			joint.connectedBody = previousRB;
-			if (i==0)
-			{
-				joint.connectedAnchor = Vector2.zero;
-				link.GetComponent<Rigidbody2D>().mass = helicopterMass;
-			}
-
-			if (i < links - 1)
-			{
-				previousRB = link.GetComponent<Rigidbody2D>();
-			} else
-			{
-				hook.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
-			}
-
-		
-
-			
+			bool isFirstLink = i == 0;
+			prevRB = AppendObjectToRope(linkPrefab, prevRB, i, linkDist, isFirstLink);
 		}
+		//last link is hook
+		AppendObjectToRope(hookPrefab, prevRB, numLinks, linkDist, false);
+	}
+
+	Rigidbody2D AppendObjectToRope(GameObject objPrefab, Rigidbody2D prevLinkRB, int linkNum, float linkDist, bool isFirstLink) {
+
+		GameObject link = (GameObject)GameObject.Instantiate (objPrefab, this.transform.parent);
+
+		Rigidbody2D rb = link.GetComponent<Rigidbody2D> ();
+		rb.mass = linkMass;
+
+		DistanceJoint2D dJoint = link.GetComponent<DistanceJoint2D>();
+		dJoint.connectedBody = helicopterRB;
+		dJoint.connectedAnchor = helicopterRopeAnchorPoint.localPosition;	//.anchor is where the anchor is on the link, connectedAnchor is where the anchor is on the helicopter RELATIVE TO THE HELICOPTER
+		//.anchor should be configured on the link prefab itself.
+		dJoint.autoConfigureDistance = false;
+		dJoint.distance = (1+linkNum) * linkDist;
+		dJoint.maxDistanceOnly = true;
+
+		HingeJoint2D hJoint = link.GetComponent<HingeJoint2D> ();
+		hJoint.connectedBody = prevLinkRB;
+		hJoint.autoConfigureConnectedAnchor = false;
+		if (isFirstLink) {
+			//hJoint anchor and connected should be in the same spot
+			hJoint.anchor = new Vector2 (0, linkDist);
+			hJoint.connectedAnchor = helicopterRopeAnchorPoint.localPosition;
+		} else {
+			//hJoint anchor and connected should be in the same spot
+			hJoint.anchor = new Vector2 (0, linkDist);	//Assumes that the rope is being generated in a straight line (x = 0)
+			hJoint.connectedAnchor = Vector2.zero;	//assumes prev link's joint is in the middle of prev link
+		}
+
+		return rb;
 	}
 
 }
